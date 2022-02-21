83a357325e55302da8eabf9f0122b4dd026b4272
jaypennington <jaypenn92@gmail.com>
2022-02-20 20:10:52 -0500

    setup repo, created account class

---
+++ src/account/account.py
@@ -0,0 +1,39 @@
+from distutils.log import error
+
+
+class Account():
+    def __init__(self):
+        self.__id = 0
+        self.__balance = 0
+
+    def __overdraft(self, amount):
+        if amount > self.__balance:
+            return True
+        else:
+            return False
+
+    def interest(self, rate):
+        self.__balance += self.__balance*rate
+
+    def get_id(self):
+        return self.__id
+
+    def get_balance(self):
+        return self.__balance
+    
+    def deposit(self, amount):
+        self.__balance += amount
+        return self.__balance
+
+    def withdrawl(self, amount):
+        if self.__overdraft(amount):
+            return(ValueError("Overdraft: Not enough funds to withdrawl!"))
+        self.__balance -= amount
+        return self.__balance
+
+    def transfer(self, account, amount):
+        if self.__overdraft(amount):
+            return(ValueError("Overdraft: Not enough funds to transfer!"))
+        self.__balance -= amount
+        account.__balance += amount
+        return self.__balance
---
+++ test/test_account.py
@@ -0,0 +1,39 @@
+import bank.src.account.account as account
+
+import random
+import time
+
+def test_create():
+    savings = account.Account()
+    assert savings.get_balance() == 0
+
+def test_deposit():
+    savings = account.Account()
+
+    random.seed(int(time.time()))
+    add = round(random.uniform(20.0, 100.0), 2)
+
+    assert savings.deposit(add) == add
+
+def test_withdrawl():
+    savings = account.Account()
+
+    random.seed(int(time.time()))
+    add = round(random.uniform(20.0, 100.0), 2)
+    remove = round(random.uniform(0.0, add), 2)
+
+    savings.deposit(add)
+    final = add - remove
+
+    assert savings.withdrawl(remove) == final
+
+def test_overdraft():
+    savings = account.Account()
+
+    random.seed(int(time.time()))
+    add = round(random.uniform(20.0, 100.0), 2)
+    remove = round(random.uniform(add, 200.0), 2)
+
+    savings.deposit(add)
+
+    assert type(savings.withdrawl(remove)) == ValueError
